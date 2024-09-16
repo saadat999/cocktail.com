837341d144be4c57c718dbe01a03d6159c5d147b
Erkeaiym <e01461485@gmail.com>
2024-09-16 15:43:47 +0600

    ghjhj

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../component/Hero'
+
+const HomePage = () => {
+    return (
+        <div>
+            iljkhjgfdsz
+            <Hero />
+        </div>
+    )
+}
+
+export default HomePage
---
+++ src/pages/SecondPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../UI/footer/Footer'
+import Header from '../UI/header/Header'
+
+const SecondPage = () => {
+    return (
+        <div>
+            <Header />
+              <div>
+
+
+                
+              </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default SecondPage
