5bc2ff221d8cf539058a0198ddd1228875488ef5
Erkeaiym <e01461485@gmail.com>
2024-09-16 19:12:24 +0600

    footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../component/Hero'
 import Header from '../UI/header/Header'
+import Footer from '../UI/footer/Footer'
 
 const HomePage = () => {
     return (
         <div>
             <Hero />
             <Header/>
+            <Footer/>
         </div>
     )
 }
